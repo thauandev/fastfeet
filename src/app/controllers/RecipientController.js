c2766c177bfde74de1466d8caa139301d102ced6
thauandev <thauandepaula@icloud.com>
2020-01-30 11:25:23 -0300

    Primeiro commit

---
+++ src/app/controllers/RecipientController.js
@@ -0,0 +1,27 @@
+import * as Yup from 'yup';
+
+import Recipient from '../models/Recipient';
+
+class RecipientController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      address: Yup.string().required(),
+      number: Yup.number().required(),
+      complement: Yup.number().required(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      zipcode: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validations fails' });
+    }
+
+    const recipient = await Recipient.create(req.body);
+
+    return res.json(recipient);
+  }
+}
+
+export default new RecipientController();
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '6d16d07a75e5c3e380afadaca69813c4',
+  expiresIn: '7d',
+};
