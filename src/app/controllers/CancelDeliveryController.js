7262fb7714c37d0405ae4e68c3dfc4dc2aae6904
thauandev <thauandepaula@icloud.com>
2020-02-17 10:29:56 -0300

    Melhorando o CancelController

--- src/app/controllers/CancelDeliveryController.js
+++ src/app/controllers/CancelDeliveryController.js
@@ -1,6 +1,11 @@
 import { startOfHour, isBefore, parseISO } from 'date-fns';
+
 import DeliveryProblem from '../models/DeliveryProblem';
 import Delivery from '../models/Delivery';
+import DeliveryMan from '../models/DeliveryMan';
+import Recipient from '../models/Recipient';
+
+import Mail from '../../lib/Mail';
 
 class CancelDeliveryController {
   async delete(req, res) {
@@ -33,11 +38,39 @@ class CancelDeliveryController {
     const delivery = await Delivery.findOne({
       where: {
         id: deliveryIndex,
+        canceled_at: null,
       },
     });
 
+    if (!delivery) {
+      res.status(401).json({ error: 'Delivery cannot be canceled' });
+    }
+
     await delivery.update({
       canceled_at: cancelDate,
+    });
+
+    const deliveryMan = await DeliveryMan.findOne({
+      where: {
+        id: delivery.deliveryman_id,
+      },
+    });
+
+    const recipient = await Recipient.findOne({
+      where: {
+        id: delivery.recipient_id,
+      },
+    });
+
+    await Mail.sendMail({
+      to: `${deliveryMan.name} <${deliveryMan.email}`,
+      subject: 'Você tem uma entrega cancelada',
+      template: 'cancelation',
+      context: {
+        deliveryman: deliveryMan.name,
+        recipient: recipient.name,
+        product: delivery.product,
+      },
     });
 
     return res.json({ messsage: 'Delivery canceled' });
